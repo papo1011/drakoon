475825dca0069459a6ebabd12b2ba4bb1ffcd5a2
papo1011 <francesco.papini.siena@gmail.com>
2025-08-04 22:58:12 +0200

    refactor: change file name

--- src/ast.rs
+++ src/ast.rs
@@ -1,29 +1,3 @@
-#[derive(Clone, Debug, PartialEq)]
-pub enum Statement {
-    Variable {
-        name: String,
-        value: Box<Expression>,
-    },
-    Print {
-        value: Box<Expression>,
-    },
-}
+mod expr;
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Expression {
-    Integer(i32),
-    Variable(String),
-    BinaryOperation {
-        lhs: Box<Expression>,
-        operator: Operator,
-        rhs: Box<Expression>,
-    },
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum Operator {
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
+pub use expr::*;
--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,7 +1,7 @@
 use drakoon::{grammar::ScriptParser, lexer::Lexer};
 
 #[test]
-fn calculator4() {
+fn expr() {
     let mbt = r#"
         let a = 42
         let b = 23
